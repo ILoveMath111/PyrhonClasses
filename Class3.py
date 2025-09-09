34086b34cec48810ec82d8aa56e1dac82ce8d9af
ILoveMath111 <sr103209@student.musd.org>
2025-09-09 19:08:57 +0530

    Add files via upload

---
+++ Class3.py
@@ -0,0 +1,48 @@
+"""Data types
+Outline:
+Write a program to find what type of datatype the given variable is?"""
+a=5
+print(type(a))
+b=7.5
+print(type(b))
+c="vasu"
+print(type(c))
+d=-4
+print(type(d))
+e=True
+print(type(e))
+
+
+
+"""Typecasting
+Outline:
+Write a program to change the datatype of given variables?"""
+name="Shouriya"
+print(type(name))
+newname=bool(name)
+print(type (newname))
+a=5
+print(type(a))
+b=bool(a)
+print(type(b))
+a=True
+print(type(a))
+b=float(a)
+print(type(b))
+
+
+"""String operation
+Outline:
+Write a program to print the “CODINGAL” word in a reverse direction."""
+a="codingal"
+reverse=a[::-1]
+print(reverse)
+b="suman"
+reverse=b[::-1]
+print(reverse)
+c="ankita"
+reverse=c[::-1]
+print(reverse)
+d="vasu"
+reverse=d[::-1]
+print(reverse)
---
+++ HomerworkClass3.py
@@ -0,0 +1,4 @@
+word = "congratulations"
+uppercase_word = word.upper()
+print(f"Original word: {word}")
+print(f"Uppercase word: {uppercase_word}")
