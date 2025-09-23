82ebccf62610db3cc702de2dbcceac1716b2c3cc
ILoveMath111 <sr103209@student.musd.org>
2025-09-23 19:01:51 +0530

    Add files via upload

---
+++ HomeworkClass7.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter a character
+character = input("Enter a character: ")
+
+# Get the ASCII value using the ord() function
+ascii_value = ord(character)
+
+# Print the character and its ASCII value
+print(f"The ASCII value of '{character}' is {ascii_value}")
