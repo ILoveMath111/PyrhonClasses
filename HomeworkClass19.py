8cb760b624fa31cb49cea762c3d4319a4a90e911
ILoveMath111 <sr103209@student.musd.org>
2025-11-09 07:03:46 +0530

    Add files via upload

---
+++ HomeworkClass19.py
@@ -0,0 +1,12 @@
+import math
+
+angle_degrees = 30
+angle_radians = math.radians(angle_degrees)
+
+sin_value = math.sin(angle_radians)
+cos_value = math.cos(angle_radians)
+tan_value = math.tan(angle_radians)
+
+print(f"The sine of {angle_degrees} degrees is: {sin_value}")
+print(f"The cosine of {angle_degrees} degrees is: {cos_value}")
+print(f"The tangent of {angle_degrees} degrees is: {tan_value}")
