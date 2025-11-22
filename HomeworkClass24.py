aad8d2e196bd7135054ce004487750b034307e54
ILoveMath111 <sr103209@student.musd.org>
2025-11-23 03:08:15 +0530

    Add files via upload

---
+++ HomeworkClass24.py
@@ -0,0 +1,7 @@
+# Define two sets with example values
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+
+# Calculate and print the symmetric difference
+symmetric_diff = set1 ^ set2
+print(f"Symmetric Difference (^ operator): {symmetric_diff}")
